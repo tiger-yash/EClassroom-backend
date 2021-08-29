4153569b005cf3c72c4603fc63de87d74002fba1
tiger-yash <tigeryashraj@gmail.com>
2021-08-29 23:33:43 +0530

    submission models

--- students/models.py
+++ students/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.conf import settings
+from classes.models import Test,Assignment
 
-# Create your models here.
+class AssignmentsSubmission(models.Model):
+    student=models.ForeignKey(
+        settings.AUTH_USER_MODEL,on_delete=models.CASCADE, related_name="student_ass")
+    assignment=models.ForeignKey(
+        Assignment,on_delete=models.CASCADE, related_name="assign")
+    submission_date=models.DateTimeField(auto_now=True)
+    url=models.URLField(max_length=200)
+
+    def __str__(self):
+        return self.assignment
+
+class TestsSubmission(models.Model):
+    student=models.ForeignKey(
+        settings.AUTH_USER_MODEL,on_delete=models.CASCADE, related_name="student_test")
+    test=models.ForeignKey(
+        Test,on_delete=models.CASCADE, related_name="quiz")
+    submission_date=models.DateTimeField(auto_now=True)
+    url=models.URLField(max_length=200)
+
+    def __str__(self):
+        return self.test
